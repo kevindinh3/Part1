652e10f764732bc3cc24258ba2f3f9d3ece4510e
kevindinh3 <104633594+kevindinh3@users.noreply.github.com>
2023-01-30 13:13:34 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,44 @@
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Blah blah blah',
+        exercises: 23,
+        id: 4
+      }
+    ]
+  }
+  
+  return <Course course={course}/>
+}
+
+const Course = (props) => {
+  return(
+    <div>
+      <Header course={props.course} />
+      <Content parts={props.course.parts} />
+      <Total parts={props.course.parts} />
+    </div>
+  )
+}
+
 const Header = (props) => {
   return (
     <h1>
@@ -9,53 +50,28 @@ const Header = (props) => {
 const AllParts = (props) => {
   return(
     <div>
-      <p>{props.parts[0].name} {props.parts[0].exercises} </p>
-      <p>{props.parts[1].name} {props.parts[1].exercises} </p>
-      <p>{props.parts[2].name} {props.parts[2].exercises} </p>
+      {props.result}
     </div>
   )
 }
 
 const Content = (props) => {
+  const result = props.parts.map(part => 
+    <p key={part.id}>{[part.name,part.exercises].join(" ")}</p>
+  )
   return (
     <div>
-      <AllParts {...props}/>
+      <AllParts parts={props.parts} result={result}/>
     </div>
-    
   )
 }
 
 const Total = (props) => {
+  const number = props.parts.map(part => part.exercises);
+  const sum = number.reduce((a,b) => a + b,0);
   return (
-    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises  + props.parts[2].exercises }</p>
+    <b>{sum}</b>
   )
 }
 
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-
-  return (
-    <div>
-      <Header course={course} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
 export default App
